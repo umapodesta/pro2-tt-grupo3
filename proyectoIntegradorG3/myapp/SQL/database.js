ccdd12c4d3199a73c7a22a77f8e7614bf1f62cb3
umapodesta <142595382+umapodesta@users.noreply.github.com>
2024-04-14 20:49:07 -0300

    Lista de patos

---
+++ proyectoIntegradorG3/myapp/SQL/database.js
@@ -0,0 +1,163 @@
+let patitos = {
+    usuarios : {
+        nombre : "Nombre del usuario",
+        email: "user@mail.com",
+        contrasenia: "********"
+    },
+    productos: [ 
+        
+        {
+            // Datos del producto 1
+            nombre: "Patito policia",
+            descripcion: "Pato de hule color amarillo con traje de policia azul. ",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 1
+                    Username: "User1",
+                    descripcion: "Comentario del producto 1",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 2
+            nombre: "Patito princesa rosada",
+            descripcion: "Pato de hule color rosado con su corona dorada (inspirado en la artista pop argetinta Martina Stoessel - TINI TINI TINI).",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 2
+                    Username: "User2",
+                    descripcion: "Comentario del producto 2",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   //Datos del producto 3
+            nombre: "Pato bombero",
+            descripcion: "Fotografía profesional en tu bolsillo. Descubrí infinitas posibilidades para tus fotos con las 3 cámaras principales de tu equipo. Poné a prueba tu creatividad y jugá con la iluminación, diferentes planos y efectos para obtener grandes resultados.",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 3
+                    Username: "User3",
+                    descripcion: "Comentario del producto 3",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 4
+            nombre: "Pato Freddy Mercury",
+            descripcion: "Pato de hule color amarillo del artista Freddy Mercury (incluye microfono y anteojos).",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 4
+                    Username: "User4",
+                    descripcion: "Comentario del producto 4",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 5
+            nombre: "Pato Hombre Arania",
+            descripcion: "Pato de hule con traje del superheroe Spiderman.",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 5
+                    Username: "User5",
+                    descripcion: "Comentario del producto 5",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 6
+            nombre: "Pato Minion",
+            descripcion: "Pato de hule color amarillo con traje de minion (incluye guantes y googles).",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 6
+                    Username: "User6",
+                    descripcion: "Comentario del producto 6",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 7
+            nombre: "Pato Donald Trump",
+            descripcion: "Pato de hule color amarillo del presidente DOnald Trump.",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 7
+                    Username: "User7",
+                    descripcion: "Comentario del producto 7",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 8
+            nombre: "Pato Toro",
+            descripcion: "Pato de hule de toro color negro con cuernos blancos.",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 8
+                    Username: "User8",
+                    descripcion: "Comentario del producto 8",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 9
+            nombre: "Pato Jocker",
+            descripcion: "Pato de hule del villano Joker con traje incluido.",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 9
+                    Username: "User9",
+                    descripcion: "Comentario del producto 9",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        },
+
+        {   // Datos del producto 10
+            nombre: "Pato Chewbacca",
+            descripcion: "Pato de hule del personaje de la saga de Star Wars Chewbacca color marron.",
+            
+
+            comentarios: [
+                {   // Comentarios del producto 10
+                    Username: "User10",
+                    descripcion: "Comentario del producto 10",
+                    imagenUsuario: "/public/images/users/defaultImage.png"
+                },
+                
+            ]
+        }
+
+
+    ]
+       
+}
