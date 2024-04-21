e79d91ba163d340c99efbe905af6d78f2401ccaa
umapodesta <142595382+umapodesta@users.noreply.github.com>
2024-04-21 12:24:04 -0300

    mini commit

--- proyectoIntegradorG3/myapp/app.js
+++ proyectoIntegradorG3/myapp/app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var patitosRouter = require('./routes/patitos');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const patitosRouter = require('./routes/patitos');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
